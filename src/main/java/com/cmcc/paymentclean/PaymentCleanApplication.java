17f18f700fd2d532553b307345504740729ab7b7
liudongyang <fan.qingli@iwhalecloud.com>
2020-08-31 17:40:05 +0800

    init code

---
+++ src/main/java/com/cmcc/paymentclean/PaymentCleanApplication.java
@@ -0,0 +1,13 @@
+package com.cmcc.paymentclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentCleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentCleanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cmcc/paymentclean/PaymentCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmcc.paymentclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentCleanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
