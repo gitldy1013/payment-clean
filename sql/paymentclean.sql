0db4d180aade8928d7535f6ed6a0673daaf87fe0
zhaolei <zhaoleids@chinamobile.com>
2020-09-08 17:40:53 +0800

    新增风控上送数据的库表

--- sql/paymentclean.sql
+++ sql/paymentclean.sql
@@ -50,3 +50,60 @@ create table pcac_risk_info
    registered_code      varchar(256) comment '商户注册号',
    primary key (pcac_risk_info_id)
 )comment = '清算协会推送黑名单和风险提示信息表 ';
+
+
+drop table if exists risk_merchant_risk_sync_info;
+
+create table risk_merchant_risk_sync_info
+(
+   cus_type             varchar(2) comment '商户类型',
+   risk_type            varchar(2) comment '风险类型',
+   cus_nature           varchar(2) comment '商户属性',
+   cus_number           varchar(20) comment '商户编号',
+   level                varchar(2) comment '风险信息等级',
+   valid_date           date comment '有效期',
+   occurtimeb           date comment '风险事件发生时间',
+   occurtimee           date comment '风险事件结束时间',
+   note                 varchar(2048) comment '风险事件描述',
+   source_channel       varchar(4) comment '风险信息来源',
+   operator             varchar(10) comment '操作人',
+   operate_time         date comment '操作时间'
+)comment = '风控商户风险信息同步表 ';
+
+
+
+drop table if exists risk_person_risk_sync_info;
+
+create table risk_person_risk_sync_info
+(
+   risk_type            varchar(2) comment '风险类型',
+   usrNo                varchar(20) comment '内部用户号',
+   valid_date           date comment '有效期',
+   occurtimeb           date comment '风险事件发生时间',
+   occurtimee           date comment '风险事件结束时间',
+   occurchan            varchar(2) comment '风险事件发生渠道',
+   note                 varchar(2048) comment '风险事件描述',
+   source_channel       varchar(4) comment '风险信息来源',
+   occurarea            varchar(256) comment '风险事件发生地域',
+   operator             varchar(10) comment '操作人',
+   operate_time         date comment '操作时间'
+)comment = '风控个人风险信息同步表 ';
+
+drop table if exists risk_enterprise_risk_sync_info;
+
+create table risk_enterprise_risk_sync_info
+(
+   risk_type            varchar(2) comment '风险类型',
+   payAccountNo         varchar(30) comment '单位支付账户编号',
+   occurtimeb           date comment '风险事件发生时间',
+   occurtimee           date comment '风险事件结束时间',
+   note                 varchar(2048) comment '风险事件描述',
+   source_channel       varchar(4) comment '风险信息来源',
+   valid_date           date comment '有效期',
+   operator             varchar(10) comment '操作人',
+   operate_time         date comment '操作时间'
+)comment = '风控企业风险信息同步表 ';
+
+
+
+
