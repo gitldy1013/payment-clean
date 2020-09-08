9d781dc8b4fa7ecb21c15d7861c0a483b332882a
zhaolei <zhaoleids@chinamobile.com>
2020-09-08 16:53:45 +0800

    新增sql

--- sql/paymentclean.sql
+++ sql/paymentclean.sql
@@ -5,7 +5,7 @@ drop table if exists pcac_assistance_info;
 create table pcac_assistance_info
 (
     pcac_assistance_info_id                   int(64) not null comment 'id序号',
-    up_date              varchar(10) comment '推送日期',
+    up_date              date comment '推送日期',
     cus_code             varchar(64) comment '商户代码',
     reg_name             varchar(64) comment '商户名称',
     leg_doc_name         varchar(128) comment '法定代表人（负责人）姓名',
@@ -24,43 +24,32 @@ create table pcac_opt_log
     primary key (pcac_opt_log_id)
 ) comment = '操作日志表 ';
 
+
 drop table if exists pcac_risk_info;
 
 create table pcac_risk_info
 (
-    pcac_risk_info_id                   int(64) not null comment 'id序号',
-    up_date              varchar(10) comment '推送日期',
-    reg_name             varbinary(128) comment '商户名称',
-    cus_name             varchar(128) comment '商户简称',
-    doc_type             varchar(2) comment '法人证件类型',
-    doc_code             varchar(64) comment '法人证件号码',
-    leg_doc_name         varchar(64) comment '法定代表人姓名',
-    leg_doc_type         varchar(2) comment '法定代表人姓名',
-    leg_doc_code         varchar(64) comment '法定代表人（负责人） 证件号码',
-    level                varchar(2) comment '风险信息等级',
-    risk_type            varchar(2) comment '风险类型',
-    valid_date           varchar(10) comment '有效期',
-    valid_status         varchar(2) comment '有效性',
-    cus_type             varchar(2) comment '商户类型',
-    occurarea            varchar(256) comment '风险事件发生地域，省级/地市，可多选，逗号隔开',
-    bank_no              varchar(128) comment '银行结算账户',
-    url                  varchar(512) comment '网址',
-    registered_code      varchar(256) comment '商户注册号',
-    primary key (pcac_risk_info_id)
-) comment '商户黑名单提示信息表';
+   pcac_risk_info_id    int(64) not null auto_increment comment 'id序号',
+   pushListType         varchar(2) comment '推送名单类型,01黑名单，02风险提示信息',
+   up_date              varchar(10) comment '推送日期',
+   reg_name             varchar(128) comment '商户名称',
+   cus_name             varchar(128) comment '商户简称',
+   doc_type             varchar(2) comment '法人证件类型',
+   doc_code             varchar(64) comment '法人证件号码',
+   leg_doc_name         varchar(64) comment '法定代表人姓名',
+   leg_doc_type         varchar(2) comment '法定代表人姓名',
+   leg_doc_code         varchar(64) comment '法定代表人（负责人） 证件号码',
+   level                varchar(2) comment '风险信息等级',
+   risk_type            varchar(2) comment '风险类型',
+   valid_date           varchar(10) comment '有效期',
+   valid_status         varchar(2) comment '有效性',
+   cus_type             varchar(2) comment '商户类型',
+   occurarea            varchar(256) comment '风险事件发生地域，省级/地市，可多选，逗号隔开',
+   bank_no              varchar(128) comment '银行结算账户',
+   url                  varchar(512) comment '网址',
+   registered_code      varchar(256) comment '商户注册号',
+   primary key (pcac_risk_info_id)
+)comment = '清算协会推送黑名单和风险提示信息表 ';
 
-create table pcac_local_risk_reg
-(
-    pcac_local_risk_reg_id                   int(64) not null comment 'id序号',
-    cus_type              varchar(2) comment '商户类型',
-    risk_type             varchar(2) comment '风险类型',
-    cus_nature            varchar(64) comment '商户属性',
-    cus_number            varchar(128) comment '商户编号',
-    level                 varchar(2) comment '风险信息等级',
-    valid_date            varchar(10) comment '有效期',
-    occurtime_b           varchar(10) comment '风险事件发生时间',
-    occurtime_e           varchar(10) comment '风险事件结束时间',
-    note                  varchar(128) comment '风险事件描述',
-    source_channel        varchar(128) comment '风险信息来源',
-    primary key (pcac_local_risk_reg_id)
-) comment '本地风险商户信息上报表';
+
+
