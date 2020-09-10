81fe8c6bd6010dca0f6d7ec024512a159f69d6d8
zhaolei <zhaoleids@chinamobile.com>
2020-09-10 15:01:31 +0800

    添加证书，修改清算协会加解密方法

--- src/main/java/com/cmcc/paymentclean/utils/CFCACipherUtil.java
+++ src/main/java/com/cmcc/paymentclean/utils/CFCACipherUtil.java
@@ -1,6 +1,7 @@
 package com.cmcc.paymentclean.utils;
 
 import cfca.sadk.algorithm.common.Mechanism;
+import cfca.sadk.algorithm.common.PKIException;
 import cfca.sadk.lib.crypto.JCrypto;
 import cfca.sadk.lib.crypto.Session;
 import cfca.sadk.util.Base64;
@@ -10,17 +11,21 @@ import cfca.sadk.util.Signature;
 import cfca.sadk.x509.certificate.X509Cert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author zhaolei
@@ -29,33 +34,42 @@ import java.util.Map;
 public class CFCACipherUtil {
 
 
-    private static final  Logger logger = LoggerFactory.getLogger(CFCACipherUtil.class);
+    private static final Logger logger = LoggerFactory.getLogger(CFCACipherUtil.class);
     // 签名证书（带私钥）
-    private static String pfxFilePath = "E:/cert/协会证书私钥证书.pfx";
+    //private static String pfxFilePath = "E:/cert/协会证书私钥证书.pfx";
     // 签名证书保护密码
-    private static String pfxFilePwd = "11111111";
+    //private static String pfxFilePwd = "11111111";
     // 签名证书公钥证书
-    private static String publicCertFilePath = "E:/cert/协会测试公钥.cer";
+    private static String publicCertFilePath = "/cert/pcac.cer";
 
     // 解密证书（带私钥）
-    private static String encPfxFilePath = "E:/cert/会员单位私钥证书.pfx";
+    private static String encPfxFilePath = "/cert/huiyuan.pfx";
     // 解密证书保护密码
-    private static String encPfxFilePwd = "11111111";
+    private static String encPfxFilePwd = "cfca1234";
     // 加密证书公钥证书
-    private static String encPublicCertFilePath = "E:/cert/会员单位公钥证书.cer";
+    private static String encPublicCertFilePath = "/cert/huiyuan.cer";
+    private static Session session = null;
+    static {
+        try {
+        JCrypto.getInstance().initialize(JCrypto.JSOFT_LIB, null);
 
+             session = JCrypto.getInstance().openSession(JCrypto.JSOFT_LIB);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     /*
      * 数据加签
-     * 发送方使用清算协会私钥对原文信息进行签名
+     * 发送方使用会员单位私钥对原文信息进行签名
      * */
     public static String doSignature(String srcData) {
         String encodedSignature = null;
         // 初始化加密会话
         try {
-            logger.info("需要加签原数据:{}",srcData);
-            JCrypto.getInstance().initialize(JCrypto.JSOFT_LIB, null);
-            Session session = JCrypto.getInstance().openSession(JCrypto.JSOFT_LIB);
-            logger.info("私钥路径:{}",encPfxFilePath);
+            logger.info("需要加签原数据:{}", srcData);
+            /*JCrypto.getInstance().initialize(JCrypto.JSOFT_LIB, null);
+            Session session = JCrypto.getInstance().openSession(JCrypto.JSOFT_LIB);*/
+            logger.debug("私钥路径:{}", encPfxFilePath);
             // 获取私钥
             PrivateKey priKey = KeyUtil.getPrivateKeyFromPFX(encPfxFilePath, encPfxFilePwd);
 
@@ -73,18 +87,19 @@ public class CFCACipherUtil {
 
     }
 
-    /*
+    /**
      * 数据验签
      * 发送方使用清算协会公钥对信息进行验签
      * */
-    public static boolean verifySignature(String srcData,String encodedSignature) {
+    public static boolean verifySignature(String srcData, String encodedSignature) {
 
-        try{
+        try {
             // 初始化加密会话
-            JCrypto.getInstance().initialize(JCrypto.JSOFT_LIB, null);
-            Session session = JCrypto.getInstance().openSession(JCrypto.JSOFT_LIB);
+            /*JCrypto.getInstance().initialize(JCrypto.JSOFT_LIB, null);
+            Session session = JCrypto.getInstance().openSession(JCrypto.JSOFT_LIB);*/
 
             // 获取清算协会公钥
+            //FileInputStream fin = new FileInputStream(publicCertFilePath);
             FileInputStream fin = new FileInputStream(publicCertFilePath);
             X509Cert cert = new X509Cert(fin);
             Signature sigUtil = new Signature();
@@ -93,7 +108,7 @@ public class CFCACipherUtil {
                 logger.info("RSA P1 verify OK!");
                 return true;
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.info("RSA P1 verify FAILER!");
             e.printStackTrace();
         }
@@ -102,13 +117,72 @@ public class CFCACipherUtil {
     }
 
 
-    /*
-     * 数据加密，AES密钥加密
+   /**
+     * 数据加密
+    * 返回map中有key为secretKey，值是加密后的AES密钥
      * */
+    public static Map<String,String> encrypt(Map<String,String> toBeEncMap) {
+        //AES加密后数据
+        String encrytedData = null;
+        //AES密钥加密后密钥key
+        String encryptedKey = null;
+        try{
+            // 初始化加密会话
+            /*JCrypto.getInstance().initialize(JCrypto.JSOFT_LIB, null);
+            Session session = JCrypto.getInstance().openSession(JCrypto.JSOFT_LIB);*/
+
+            // 生成对称AES密钥
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(128, new SecureRandom());
+            SecretKey secretKey = kgen.generateKey();
+            byte[] symmetricKeyEncoded = secretKey.getEncoded();
+            SecretKeySpec key = new SecretKeySpec(symmetricKeyEncoded, "AES");
+
+            // 对称加密
+            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
+
+            Set<String> keySet = toBeEncMap.keySet();
+            Iterator<String> iterator = keySet.iterator();
+            while (iterator.hasNext()){
+                String mapkey = iterator.next();
+                String toBeEncData = toBeEncMap.get(mapkey);
+                byte[] byteContent = toBeEncData.getBytes("UTF-8");
+                cipher.init(Cipher.ENCRYPT_MODE, key);
+                byte[] result = cipher.doFinal(byteContent);
+
+                encrytedData = new String(Base64.encode(result));
+                logger.info("使用AES对称密钥加密后的密文（经Base64编码）:{}" , encrytedData);
+                toBeEncMap.put(mapkey,encrytedData);
+
+            }
+
+
+
+            // 获取接收方公钥
+            //FileInputStream fin = new FileInputStream(publicCertFilePath);
+            FileInputStream fin = new FileInputStream(publicCertFilePath);
+            X509Cert cert = new X509Cert(fin);
+
+            // 去掉外层Base64编码，在方法体内部已经做过Base64编码
+            encryptedKey = new String(EncryptUtil.encryptMessageByRSA(symmetricKeyEncoded, cert, session));
+            logger.info("使用对方公钥加密后的会话密钥密文（经Base64编码）:{}" ,encryptedKey);
+            toBeEncMap.put("secretKey",encryptedKey);
+        }catch(Exception e){
+            logger.info("加密数据失败");
+            e.printStackTrace();
+        }
+        return toBeEncMap;
+    }
+
+
+
+    /* *//*
+     * 数据加密，AES密钥加密
+     * *//*
     public static Map<String,String> encrypt(String toBeEncData) {
         //AES加密后数据
         String encrytedData = null;
-        //AES密钥加密后数据
+        //AES密钥加密后密钥key
         String encryptedKey = null;
         Map<String,String> encryptMap = new HashMap<String,String>();
         try{
@@ -134,9 +208,12 @@ public class CFCACipherUtil {
             logger.info("使用AES对称密钥加密后的密文（经Base64编码）:{}" , encrytedData);
 
             // 获取接收方公钥
-            FileInputStream fin = new FileInputStream(publicCertFilePath);
+            //FileInputStream fin = new FileInputStream(publicCertFilePath);
+            FileInputStream fin = new FileInputStream(encPublicCertFilePath);
             X509Cert cert = new X509Cert(fin);
-            /*********************************************/
+
+
+    *//*********************************************//*
             // 去掉外层Base64编码，在方法体内部已经做过Base64编码
             encryptedKey = new String(EncryptUtil.encryptMessageByRSA(symmetricKeyEncoded, cert, session));
             logger.info("使用对方公钥加密后的会话密钥密文（经Base64编码）:{}" ,encryptedKey);
@@ -152,32 +229,41 @@ public class CFCACipherUtil {
 
     /*
      * 数据解密
+     * secretKey为加密后的AES密钥
+     * toBeDecMap为需要加密的字段map集合
      * */
-    public static String decrypt(String encryptedKey,String  encrytedData) {
-        String decryptedData =null;
-        try{
+    public static Map<String,String> decrypt(String secretKey, Map<String,String> toBeDecMap) {
+
+        try {
             // 初始化加密会话
-            JCrypto.getInstance().initialize(JCrypto.JSOFT_LIB, null);
-            Session session = JCrypto.getInstance().openSession(JCrypto.JSOFT_LIB);
+            /*JCrypto.getInstance().initialize(JCrypto.JSOFT_LIB, null);
+            Session session = JCrypto.getInstance().openSession(JCrypto.JSOFT_LIB);*/
 
             // 解密对称密钥
             PrivateKey priKey = KeyUtil.getPrivateKeyFromPFX(encPfxFilePath, encPfxFilePwd);
             /*********************************************/
             // 去掉外层Base64解码，在方法体内部已经做过Base64解码
-            byte[] keyData = EncryptUtil.decryptMessageByRSA(encryptedKey.getBytes(), priKey, session);
+            byte[] keyData = EncryptUtil.decryptMessageByRSA(secretKey.getBytes(), priKey, session);
             SecretKeySpec symmetricKey = new SecretKeySpec(keyData, "AES");
 
             // 解密被加密的项，可使用对称密钥对多个加密项进行解密
             Cipher cipher = Cipher.getInstance("AES");// 创建密码器
             cipher.init(Cipher.DECRYPT_MODE, symmetricKey);
-            byte[] result = cipher.doFinal(Base64.decode(encrytedData));
-            decryptedData = new String(result,"UTF-8");
-            logger.info("解密后的原文数据\r\n:{}",decryptedData);
-        }catch(Exception e){
+            Set<String> keySet = toBeDecMap.keySet();
+            Iterator<String> iterator = keySet.iterator();
+            while (iterator.hasNext()) {
+                String mapkey = iterator.next();
+                String encrytedData = toBeDecMap.get(mapkey);
+                byte[] result = cipher.doFinal(Base64.decode(encrytedData));
+                String  decryptedData = new String(result, "UTF-8");
+                logger.info("解密后的原文数据:{}", decryptedData);
+                toBeDecMap.put(mapkey,decryptedData);
+            }
+        } catch (Exception e) {
             logger.info("解密数据失败");
             e.printStackTrace();
         }
-        return decryptedData;
+        return toBeDecMap;
     }
 
 
