c8f8320074a14cc74be258ae0daa04dda0b5bdf9
zhaolei <zhaoleids@chinamobile.com>
2020-09-11 23:33:28 +0800

    添加内部加解密异常类

---
+++ src/main/java/com/cmcc/paymentclean/exception/InnerCipherException.java
@@ -0,0 +1,33 @@
+package com.cmcc.paymentclean.exception;
+
+/**
+ * @author zhaolei
+ * @date 2020-09-11 22:58
+ */
+public class InnerCipherException extends Exception {
+
+
+    private Integer respCode;
+    private String respDesc;
+
+    public InnerCipherException(Integer respCode,String respDesc) {
+        this.respCode = respCode;
+        this.respDesc = respDesc;
+    }
+
+    public InnerCipherException() {
+        super();
+    }
+
+    public InnerCipherException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return "InnerCipherException{" +
+                "respCode=" + respCode +
+                ", respDesc='" + respDesc + '\'' +
+                '}';
+    }
+}
