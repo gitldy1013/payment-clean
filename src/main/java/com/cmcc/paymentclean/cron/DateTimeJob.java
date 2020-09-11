e0b7899765e67da8a57db20cf8b84ca86624235b
zhaolei <zhaoleids@chinamobile.com>
2020-09-11 10:11:36 +0800

    将继承QuartzJobBean改为实现Job，避免继承局限性

--- src/main/java/com/cmcc/paymentclean/cron/DateTimeJob.java
+++ src/main/java/com/cmcc/paymentclean/cron/DateTimeJob.java
@@ -1,20 +1,28 @@
 package com.cmcc.paymentclean.cron;
 
+import org.quartz.Job;
+import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 /**
- * @author zhaolei
+ * @author zhaolei    extends QuartzJobBean
  * @date 2020-09-10 21:18
  */
-public class DateTimeJob extends QuartzJobBean {
-    @Override
+public class DateTimeJob  implements Job {
+  /*  @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         //获取JobDetail中关联的数据
-      /*  String msg = (String) jobExecutionContext.getJobDetail().getJobDataMap().get("msg");
+      *//*  String msg = (String) jobExecutionContext.getJobDetail().getJobDataMap().get("msg");
         System.out.println("current time :"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "---" + msg);
-*/
+*//*
+        System.out.println("定时任务执行内容。。。。");
+    }*/
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         System.out.println("定时任务执行内容。。。。");
     }
+
 }
