2e02ba6005a86ee5fcc36a61df8f98850a20418b
peng.xiang <peng.xiang@iwhalecloud.com>
2020-09-16 15:04:53 +0800

    测试联调

--- src/main/java/com/cmcc/paymentclean/entity/dto/resquest/RiskMerchantRiskSyncInfoReq.java
+++ src/main/java/com/cmcc/paymentclean/entity/dto/resquest/RiskMerchantRiskSyncInfoReq.java
@@ -1,6 +1,7 @@
 package com.cmcc.paymentclean.entity.dto.resquest;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -40,14 +41,17 @@ public class RiskMerchantRiskSyncInfoReq implements Serializable {
 
     @NotNull(message = "有效期不能为空")
     @ApiModelProperty(value = "有效期")
+    @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd")
     private Date validDate;
 
     @NotNull(message = "风险事件发生时间不能为空")
     @ApiModelProperty(value = "风险事件发生时间")
+    @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd")
     private Date occurtimeb;
 
     @NotNull(message = "风险事件结束时间不能为空")
     @ApiModelProperty(value = "风险事件结束时间")
+    @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd")
     private Date occurtimee;
 
     @ApiModelProperty(value = "风险事件描述")
