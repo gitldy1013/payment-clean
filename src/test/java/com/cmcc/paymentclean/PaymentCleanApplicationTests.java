956f77d32b50a999bff262588d0d72085f6cecd2
zhaolei <zhaoleids@chinamobile.com>
2020-09-17 17:20:08 +0800

    提交测试类

--- src/test/java/com/cmcc/paymentclean/PaymentCleanApplicationTests.java
+++ src/test/java/com/cmcc/paymentclean/PaymentCleanApplicationTests.java
@@ -1,7 +1,10 @@
 package com.cmcc.paymentclean;
 
+import com.cmcc.paymentclean.controller.PcacRiskInfoPushController;
 import com.cmcc.paymentclean.cron.SubmitPcacPersonRiskInfo;
 import com.cmcc.paymentclean.entity.LoginResult;
+import com.cmcc.paymentclean.entity.dto.pcac.resq.Document;
+import com.cmcc.paymentclean.entity.dto.pcac.resq.RiskInfo;
 import com.cmcc.paymentclean.service.LoginPcacService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,21 +17,43 @@ class PaymentCleanApplicationTests {
     private SubmitPcacPersonRiskInfo submitPcacPersonRiskInfo;
     @Autowired
     private LoginPcacService loginPcacService;
+
+    @Autowired
+    private PcacRiskInfoPushController pcacRiskInfoPushController;
     @Test
     void contextLoads() {
     }
 
+
+    /**
+     * 测试个人风险信息上报接口
+     *
+     * */
     @Test
     public void  test01(){
         submitPcacPersonRiskInfo.submit();
     }
 
+
+    /**
+     * 测试登录登出接口
+     * */
     @Test
     public void  test02(){
        LoginResult result = loginPcacService.login();
        // LoginResult result = loginPcacService.logout();
         System.out.println("----------------登录结果--------------");
         System.out.println(result);
+    }
+
+    /**
+     *
+     * 测试黑名单推送，风险提示信息推送
+     * */
+    public  void test03(){
+
+        String doxml = "";
+        pcacRiskInfoPushController.riskTipsInfo(doxml);
     }
 
 }
