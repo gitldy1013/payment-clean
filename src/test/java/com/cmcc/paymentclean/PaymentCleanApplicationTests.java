a03ebb0c658cf885b3167f5cf3eb4f7e790bb04c
zhaolei <zhaoleids@chinamobile.com>
2020-09-21 16:51:54 +0800

    提交测试类

--- src/test/java/com/cmcc/paymentclean/PaymentCleanApplicationTests.java
+++ src/test/java/com/cmcc/paymentclean/PaymentCleanApplicationTests.java
@@ -47,10 +47,12 @@ class PaymentCleanApplicationTests {
     /**
      * 测试黑名单推送，风险提示信息推送
      */
+    @Test
     public void test03() {
 
-        String doxml = "";
-        pcacRiskInfoPushController.riskTipsInfo(doxml);
+        String doxml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Document><Request><Head><Version>V1.3.0</Version><Identification>202009171000001662</Identification><OrigSender>Z2004343000017</OrigSender><OrigSenderSID>zf_sysstem</OrigSenderSID><RecSystemId>R0001</RecSystemId><TrnxCode>TS0002</TrnxCode><TrnxTime>20200917124036</TrnxTime><UserToken>Io2lrHofKybUc%2BmKeG6qo%2FMQwXMQTOQ6IwrnIfgvhyHTf2SAN63W0wKbw%2FODal5I</UserToken><SecretKey></SecretKey></Head><Body><PcacList><Count>2</Count><UpDate>2020-09-17</UpDate><RiskInfo><RegName>1</RegName><CusName></CusName><DocType>1</DocType><DocCode></DocCode><LegDocName></LegDocName><LegDocType>2020-09-27</LegDocType><LegDocCode>2020-09-17 12:40:36</LegDocCode><Level>1</Level><RiskType>1</RiskType><ValidDate>1</ValidDate><CusType></CusType><Occurarea></Occurarea><BankNo></BankNo><Url></Url><RegisteredCode></RegisteredCode></RiskInfo></PcacList></Body></Request></Document>";
+        String s = pcacRiskInfoPushController.blackListAndTipsInfo(doxml);
+        System.out.println(s);
     }
 
 }
