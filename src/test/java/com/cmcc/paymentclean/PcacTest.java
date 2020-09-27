0bf5c5a24a3082aa020a07cc4f6a9767fd182736
刘东阳 <liudongyangds@chinamobile.com>
2020-09-27 10:01:20 +0800

    接口联调

--- src/test/java/com/cmcc/paymentclean/PcacTest.java
+++ src/test/java/com/cmcc/paymentclean/PcacTest.java
@@ -1,7 +1,6 @@
 package com.cmcc.paymentclean;
 
 import com.cmcc.paymentclean.config.PcacConfig;
-import com.cmcc.paymentclean.consts.TrnxCodeEnum;
 import com.cmcc.paymentclean.entity.dto.pcac.resq.gen.pcac003.Body;
 import com.cmcc.paymentclean.entity.dto.pcac.resq.gen.pcaclogin.Document;
 import com.cmcc.paymentclean.entity.dto.pcac.resq.gen.pcaclogin.Request;
@@ -25,37 +24,37 @@ public class PcacTest<T> {
 
     @Test
     void pcacTest() {
-        //拼装Body
+        //拼装Body QR0001-个人风险信息查询
         Body body = new Body();
-        body.setRiskType("");
-        body.setMobileNo("");
-        body.setMac("");
-        body.setImei("");
-        body.setBankNo("");
-        body.setOpenBank("");
-        body.setCusName("");
-        body.setDocType("");
-        body.setDocCode("");
-        body.setIp("");
-        body.setAddress("");
-        body.setTelephone("");
-        body.setRecBankNo("");
-        body.setRecOpenBank("");
-        body.setEmail("");
-        body.setOccurtimeb("");
-        body.setOccurtimee("");
-        body.setOccurchan("");
-        body.setOccurarea("");
-        body.setRecHostArea("");
-        body.setScope("");
-        body.setValidStatus("");
-        pushPcac((T)body,"003");
+        body.setRiskType("123");
+        body.setMobileNo("123");
+        body.setMac("123");
+        body.setImei("123");
+        body.setBankNo("123");
+        body.setOpenBank("123");
+        body.setCusName("123");
+        body.setDocType("123");
+        body.setDocCode("123");
+        body.setIp("123");
+        body.setAddress("123");
+        body.setTelephone("123");
+        body.setRecBankNo("123");
+        body.setRecOpenBank("123");
+        body.setEmail("123");
+        body.setOccurtimeb("123");
+        body.setOccurtimee("123");
+        body.setOccurchan("123");
+        body.setOccurarea("123");
+        body.setRecHostArea("123");
+        body.setScope("123");
+        body.setValidStatus("123");
+        pushPcac((T) body, "003", "QR0001");
 
     }
 
-    public void pushPcac(T body, String code) {
+    public void pushPcac(T body, String code, String trnxCode) {
 
-        Document<T> document = getDocument(body);
+        Document<T> document = getDocument(body, trnxCode);
         //报文转换
         String xml = XmlJsonUtils.convertObjectToXmlStr(document);
         log.info("获取到的xml数据:{}", xml);
@@ -64,7 +63,7 @@ public class PcacTest<T> {
             return;
         }
         //校验xml报文
-        boolean validate = ValidateUtils.validateXMLByXSD(xml, "pcac.ries."+code);
+        boolean validate = ValidateUtils.validateXMLByXSD(xml, "pcac.ries." + code);
         log.info("请求报文: {}", XmlJsonUtils.formatXml(xml));
         if (!validate) {
             log.info("XML校验失败");
@@ -79,12 +78,12 @@ public class PcacTest<T> {
         log.info("返回状态码及信息：{}:{}", doc.getRespone().getBody().getRespInfo().getResultCode(), doc.getRespone().getBody().getRespInfo().getMsgDetail());
     }
 
-    private Document<T> getDocument(T body) {
+    private Document<T> getDocument(T body, String trnxCode) {
         //拼装报文
         Document<T> document = new Document<>();
         byte[] symmetricKeyEncoded = CFCACipherUtils.getSymmetricKeyEncoded();
         //设置报文头
-        Request<T> request = XmlJsonUtils.getRequest(symmetricKeyEncoded, document, pcacConfig, TrnxCodeEnum.MERCHANT_RISK_INFO_SUBMIT.getCode());
+        Request<T> request = XmlJsonUtils.getRequest(symmetricKeyEncoded, document, pcacConfig, trnxCode);
         //设置报文体
         request.setBody(body);
         document.setRequest(request);
