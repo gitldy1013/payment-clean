2dde6149e63fb535ad8d5c24542c15ec96f7ff52
刘东阳 <liudongyangds@chinamobile.com>
2020-10-19 14:43:30 +0800

    引入缓存

--- src/main/java/com/cmcc/paymentclean/config/LoginRunner.java
+++ src/main/java/com/cmcc/paymentclean/config/LoginRunner.java
@@ -1,6 +1,5 @@
 package com.cmcc.paymentclean.config;
 
-import com.cmcc.paymentclean.entity.LoginResult;
 import com.cmcc.paymentclean.service.LoginPcacService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
