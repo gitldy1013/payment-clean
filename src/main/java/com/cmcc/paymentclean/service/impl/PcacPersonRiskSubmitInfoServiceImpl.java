adcb46cfc7fd0011a4d2772c123fb37eb696028f
zhaolei <zhaoleids@chinamobile.com>
2020-11-11 17:16:54 +0800

    修改个人风险信息上报逻辑

--- src/main/java/com/cmcc/paymentclean/service/impl/PcacPersonRiskSubmitInfoServiceImpl.java
+++ src/main/java/com/cmcc/paymentclean/service/impl/PcacPersonRiskSubmitInfoServiceImpl.java
@@ -48,8 +48,8 @@ import java.util.List;
 @Slf4j
 @Service
 public class PcacPersonRiskSubmitInfoServiceImpl
-    extends ServiceImpl<PcacPersonRiskSubmitInfoMapper, PcacPersonRiskSubmitInfo>
-    implements PcacPersonRiskSubmitInfoService {
+        extends ServiceImpl<PcacPersonRiskSubmitInfoMapper, PcacPersonRiskSubmitInfo>
+        implements PcacPersonRiskSubmitInfoService {
 
   @Autowired private PcacPersonRiskSubmitInfoMapper pcacPersonRiskSubmitInfoMapper;
 
@@ -60,23 +60,23 @@ public class PcacPersonRiskSubmitInfoServiceImpl
     log.info("pageRiskPerson req={}", com.alibaba.fastjson.JSON.toJSON(riskPersonReq));
     ResultBean<Page<RiskPersonResp>> resultBean = new ResultBean<>();
     Page<PcacPersonRiskSubmitInfo> page =
-        new Page<>(riskPersonReq.getPageNo(), riskPersonReq.getPageSize());
+            new Page<>(riskPersonReq.getPageNo(), riskPersonReq.getPageSize());
     Page<RiskPersonResp> pcacPersonRiskSubmitInfoPage =
-        pcacPersonRiskSubmitInfoMapper.pagePcacPersonRiskSubmitInfo(page, riskPersonReq);
+            pcacPersonRiskSubmitInfoMapper.pagePcacPersonRiskSubmitInfo(page, riskPersonReq);
     List<RiskPersonResp> riskPersonResps = pcacPersonRiskSubmitInfoPage.getRecords();
     if (!CollectionUtils.isEmpty(riskPersonResps)) {
       for (RiskPersonResp riskPersonResp : riskPersonResps) {
         String validStatus =
-            (new Date().before(riskPersonResp.getValidDate()))
-                ? CommonConst.VALIDSTATUS_01
-                : CommonConst.VALIDSTATUS_02;
+                (new Date().before(riskPersonResp.getValidDate()))
+                        ? CommonConst.VALIDSTATUS_01
+                        : CommonConst.VALIDSTATUS_02;
         riskPersonResp.setValidStatus(validStatus);
         riskPersonResp.setSubmitStatus(
-            SubmitStatusEnum.getSubmitStatusEnumDesc(riskPersonResp.getSubmitStatus()));
+                SubmitStatusEnum.getSubmitStatusEnumDesc(riskPersonResp.getSubmitStatus()));
         riskPersonResp.setCusProperty(
-            CusPropertyEnum.getCusPropertyEnum(riskPersonResp.getCusProperty()));
+                CusPropertyEnum.getCusPropertyEnum(riskPersonResp.getCusProperty()));
         riskPersonResp.setSourceChannel(
-            SourChaEnum.getSourChaEnum(riskPersonResp.getSourceChannel()));
+                SourChaEnum.getSourChaEnum(riskPersonResp.getSourceChannel()));
         riskPersonResp.setRiskType(RiskTypeEnum.getRiskTypeDesc(riskPersonResp.getRiskType()));
       }
     }
@@ -84,7 +84,7 @@ public class PcacPersonRiskSubmitInfoServiceImpl
     resultBean.setResMsg(ResultCodeEnum.SUCCESS.getDesc());
     resultBean.setData(pcacPersonRiskSubmitInfoPage);
     log.info(
-        "pageRiskPerson resp={}", com.alibaba.fastjson.JSON.toJSON(pcacPersonRiskSubmitInfoPage));
+            "pageRiskPerson resp={}", com.alibaba.fastjson.JSON.toJSON(pcacPersonRiskSubmitInfoPage));
     return resultBean;
   }
 
@@ -92,7 +92,7 @@ public class PcacPersonRiskSubmitInfoServiceImpl
   @Override
   public void submit() {
     List<PcacPersonRiskSubmitInfo> pcacPersonRiskList =
-        pcacPersonRiskSubmitInfoMapper.selectPcacPersonRiskSubmitInfoList();
+            pcacPersonRiskSubmitInfoMapper.selectPcacPersonRiskSubmitInfoList();
     Date date = new Date();
     log.info("查询个人风险信息结果：{}", pcacPersonRiskList);
     if (pcacPersonRiskList.size() == 0) {
@@ -104,11 +104,12 @@ public class PcacPersonRiskSubmitInfoServiceImpl
     // String secretKey = CFCACipherUtils.getSecretKey(symmetricKeyEncoded);
 
     log.debug("-------开始封装xml报文实体对象------");
-    ArrayList<RiskInfo> riskInfos = new ArrayList<>();
+
 
     for (PcacPersonRiskSubmitInfo pcacPersonRiskSubmitInfo : pcacPersonRiskList) {
+      ArrayList<RiskInfo> riskInfos = new ArrayList<>();
       pcacPersonRiskSubmitInfo.setDocType(
-          PerdocTypeEnum.getPerdocTypeEnumDesc(pcacPersonRiskSubmitInfo.getDocType()));
+              PerdocTypeEnum.getPerdocTypeEnumDesc(pcacPersonRiskSubmitInfo.getDocType()));
       RiskInfo riskInfo = new RiskInfo();
       BeanUtilsEx.copyProperties(riskInfo, pcacPersonRiskSubmitInfo);
       // 上报日期库里有就不加了，没有的再自己生成
@@ -126,99 +127,102 @@ public class PcacPersonRiskSubmitInfoServiceImpl
       bankList.setCount("1");
       riskInfo.setBankList(bankList);
       riskInfos.add(riskInfo);
-    }
-    PcacList pcacLists = new PcacList();
-    pcacLists.setCount(riskInfos.size() + "");
-    pcacLists.setRiskInfo(riskInfos);
-    Body body = new Body();
-    body.setPcacList(pcacLists);
-    Document document = new Document();
-    Request request =
-        XmlJsonUtils.getRequest(
-            symmetricKeyEncoded,
-            document,
-            pcacConfig,
-            TrnxCodeEnum.PERSON_RISK_INFO_SUBMIT.getCode());
-    request.setBody(body);
-    document.setRequest(request);
-    // 注意生成签名的时候设置空串显示<Signature>,不设置空串默认null，就不显示<Signature>
-    // document.setSignature("");
-    document.setSignature("");
-    String doXml = XmlJsonUtils.convertObjectToXmlStr(document);
-    log.info("个人风险信息上报支付清算协会请求xml报文：{}", doXml);
-    try {
-      boolean validate = ValidateUtils.validateXMLByXSD(doXml, "pcac.ries.001");
-      if (validate) {
 
-        Document encrDocument = BeanUtilsEx.getEncrBean(document, symmetricKeyEncoded);
-        encrDocument.setSignature(null);
-        String noSignXml = XmlJsonUtils.convertObjectToXmlStr(encrDocument);
-        String signature = CFCACipherUtils.doSignature(noSignXml);
-        encrDocument.setSignature(signature);
-        doXml = XmlJsonUtils.convertObjectToXmlStr(document);
+      PcacList pcacLists = new PcacList();
+      pcacLists.setCount(riskInfos.size() + "");
+      pcacLists.setRiskInfo(riskInfos);
+      Body body = new Body();
+      body.setPcacList(pcacLists);
+      Document document = new Document();
+      Request request =
+              XmlJsonUtils.getRequest(
+                      symmetricKeyEncoded,
+                      document,
+                      pcacConfig,
+                      TrnxCodeEnum.PERSON_RISK_INFO_SUBMIT.getCode());
+      request.setBody(body);
+      document.setRequest(request);
+      // 注意生成签名的时候设置空串显示<Signature>,不设置空串默认null，就不显示<Signature>
+      // document.setSignature("");
+      document.setSignature("");
+      String doXml = XmlJsonUtils.convertObjectToXmlStr(document);
+      log.info("个人风险信息上报支付清算协会请求xml报文：{}", doXml);
+      try {
+        boolean validate = ValidateUtils.validateXMLByXSD(doXml, "pcac.ries.001");
+        if (validate) {
 
-        log.info("----------------------------------------------打印个人风险信息上报参数：--------");
-        String s = XmlJsonUtils.formatXml(doXml);
-        log.info(s);
-        log.info("----------------------------------------------打印个人风险信息上报参数：--------");
-        // String result =
-        // HttpClientUtils.sendHttpsPost("http://210.12.239.161:10001/ries_interface/httpServlet",
-        // doXml);
-        String result = HttpClientUtils.sendHttpsPost(pcacConfig.getUrl(), doXml);
-        log.info("----------------------------------------------打印个人风险信息响应参数：--------");
-        String ss = XmlJsonUtils.formatXml(result);
-        log.info(ss);
-        log.info("----------------------------------------------打印个人风险信息响应参数：--------");
-        log.info("个人风险信息上报支付清算协会响应xml报文：", result);
-        com.cmcc.paymentclean.entity.dto.pcac.resp.Document documentResp =
-            (com.cmcc.paymentclean.entity.dto.pcac.resp.Document)
-                com.cmcc.paymentclean.utils.XmlJsonUtils.convertXmlStrToObject(
-                    result, com.cmcc.paymentclean.entity.dto.pcac.resp.Document.class);
-        String signatureResp = documentResp.getSignature();
-        log.info("响应报文的签名串signature：{}", signatureResp);
-        documentResp.setSignature(null);
-        String noSignXmlResp = XmlJsonUtils.convertObjectToXmlStr(documentResp);
-        log.info("不加签名信息的响应报文xml串：{}", noSignXmlResp);
-        boolean isSign = CFCACipherUtils.verifySignature(noSignXmlResp, signatureResp);
-        //  if (isSign) {
-        Respone respone = documentResp.getRespone();
-        RespInfo respInfo = respone.getBody().getRespInfo();
-        if (PcacResultCodeEnum.S00000.getCode().equals(respInfo.getResultCode())
-            && "01".equals(respInfo.getResultStatus())) {
-          // 上报成功，修改数据库状态
-          PcacPersonRiskSubmitInfo pcacPersonRiskSubmitInfo = new PcacPersonRiskSubmitInfo();
-          BeanUtils.copyProperties(respInfo, pcacPersonRiskSubmitInfo);
-          pcacPersonRiskSubmitInfo.setSubmitTime(date);
-          pcacPersonRiskSubmitInfo.setSubmitStatus("1");
-          pcacPersonRiskSubmitInfo.setMsgDetail("已上报");
-          pcacPersonRiskSubmitInfo.setRepDate(date);
-          log.info("更新数据库表时间和状态信息：{}", pcacPersonRiskSubmitInfo);
-          pcacPersonRiskSubmitInfoMapper.updateByPcacPersonRiskSubmitInfo(pcacPersonRiskSubmitInfo);
-        } else {
-          // 上报失败，修改数据库状态
-          PcacPersonRiskSubmitInfo pcacPersonRiskSubmitInfo = new PcacPersonRiskSubmitInfo();
-          BeanUtils.copyProperties(respInfo, pcacPersonRiskSubmitInfo);
-          pcacPersonRiskSubmitInfo.setSubmitTime(date);
-          pcacPersonRiskSubmitInfo.setSubmitStatus("0");
-          if(!StringUtils.isEmpty(respInfo.getMsgDetail())){
-            pcacPersonRiskSubmitInfo.setMsgDetail(respInfo.getMsgDetail());
-          }else {
-            pcacPersonRiskSubmitInfo.setMsgDetail(PcacResultCodeEnum.getPcacResultCodeEnum(respInfo.getResultCode()));
+          Document encrDocument = BeanUtilsEx.getEncrBean(document, symmetricKeyEncoded);
+          encrDocument.setSignature(null);
+          String noSignXml = XmlJsonUtils.convertObjectToXmlStr(encrDocument);
+          String signature = CFCACipherUtils.doSignature(noSignXml);
+          encrDocument.setSignature(signature);
+          doXml = XmlJsonUtils.convertObjectToXmlStr(document);
+
+          log.info("----------------------------------------------打印个人风险信息上报参数：--------");
+          String s = XmlJsonUtils.formatXml(doXml);
+          log.info(s);
+          log.info("----------------------------------------------打印个人风险信息上报参数：--------");
+          // String result =
+          // HttpClientUtils.sendHttpsPost("http://210.12.239.161:10001/ries_interface/httpServlet",
+          // doXml);
+          String result = HttpClientUtils.sendHttpsPost(pcacConfig.getUrl(), doXml);
+          log.info("----------------------------------------------打印个人风险信息响应参数：--------");
+          String ss = XmlJsonUtils.formatXml(result);
+          log.info(ss);
+          log.info("----------------------------------------------打印个人风险信息响应参数：--------");
+          log.info("个人风险信息上报支付清算协会响应xml报文：", result);
+          com.cmcc.paymentclean.entity.dto.pcac.resp.Document documentResp =
+                  (com.cmcc.paymentclean.entity.dto.pcac.resp.Document)
+                          com.cmcc.paymentclean.utils.XmlJsonUtils.convertXmlStrToObject(
+                                  result, com.cmcc.paymentclean.entity.dto.pcac.resp.Document.class);
+          String signatureResp = documentResp.getSignature();
+          log.info("响应报文的签名串signature：{}", signatureResp);
+          documentResp.setSignature(null);
+          String noSignXmlResp = XmlJsonUtils.convertObjectToXmlStr(documentResp);
+          log.info("不加签名信息的响应报文xml串：{}", noSignXmlResp);
+          boolean isSign = CFCACipherUtils.verifySignature(noSignXmlResp, signatureResp);
+          //  if (isSign) {
+          Respone respone = documentResp.getRespone();
+          RespInfo respInfo = respone.getBody().getRespInfo();
+          if (PcacResultCodeEnum.S00000.getCode().equals(respInfo.getResultCode())
+                  && "01".equals(respInfo.getResultStatus())) {
+            // 上报成功，修改数据库状态
+            PcacPersonRiskSubmitInfo pcacPersonRiskSubmitInfo1 = new PcacPersonRiskSubmitInfo();
+            BeanUtils.copyProperties(respInfo, pcacPersonRiskSubmitInfo1);
+            pcacPersonRiskSubmitInfo1.setSubmitTime(date);
+            pcacPersonRiskSubmitInfo1.setSubmitStatus("1");
+            pcacPersonRiskSubmitInfo1.setMsgDetail("");
+            pcacPersonRiskSubmitInfo1.setRepDate(date);
+            pcacPersonRiskSubmitInfo1.setPcacPersonRiskSubmitInfoId(pcacPersonRiskSubmitInfo.getPcacPersonRiskSubmitInfoId());
+            log.info("更新数据库表时间和状态信息：{}", pcacPersonRiskSubmitInfo1);
+            pcacPersonRiskSubmitInfoMapper.updateByPcacPersonRiskSubmitInfo(pcacPersonRiskSubmitInfo1);
+          } else {
+            // 上报失败，修改数据库状态
+            PcacPersonRiskSubmitInfo pcacPersonRiskSubmitInfo1 = new PcacPersonRiskSubmitInfo();
+            BeanUtils.copyProperties(respInfo, pcacPersonRiskSubmitInfo1);
+            pcacPersonRiskSubmitInfo1.setSubmitTime(date);
+            pcacPersonRiskSubmitInfo1.setSubmitStatus("1");
+            if(!StringUtils.isEmpty(respInfo.getMsgDetail())){
+              pcacPersonRiskSubmitInfo1.setMsgDetail(respInfo.getMsgDetail().replaceAll("个人风险信息,第1条数据:",""));
+            }else {
+              pcacPersonRiskSubmitInfo1.setMsgDetail(PcacResultCodeEnum.getPcacResultCodeEnum(respInfo.getResultCode()));
+            }
+            pcacPersonRiskSubmitInfo1.setRepDate(date);
+            pcacPersonRiskSubmitInfo1.setPcacPersonRiskSubmitInfoId(pcacPersonRiskSubmitInfo.getPcacPersonRiskSubmitInfoId());
+            log.info("更新数据库表时间和状态信息：{}", pcacPersonRiskSubmitInfo1);
+            pcacPersonRiskSubmitInfoMapper.updateByPcacPersonRiskSubmitInfo(pcacPersonRiskSubmitInfo1);
           }
-          pcacPersonRiskSubmitInfo.setRepDate(date);
-          log.info("更新数据库表时间和状态信息：{}", pcacPersonRiskSubmitInfo);
-          pcacPersonRiskSubmitInfoMapper.updateByPcacPersonRiskSubmitInfo(pcacPersonRiskSubmitInfo);
+
+        } else {
+          log.info("----------xsd文件校验xml格式失败-------");
+          throw new SubmitPCACException(
+                  ResultCodeEnum.XSD_FILE_VALID_FALSE.getCode(),
+                  ResultCodeEnum.XSD_FILE_VALID_FALSE.getDesc());
         }
 
-      } else {
-        log.info("----------xsd文件校验xml格式失败-------");
-        throw new SubmitPCACException(
-            ResultCodeEnum.XSD_FILE_VALID_FALSE.getCode(),
-            ResultCodeEnum.XSD_FILE_VALID_FALSE.getDesc());
+      } catch (Exception e) {
+        log.error("异常:" + e);
       }
-
-    } catch (Exception e) {
-      log.error("异常:" + e);
     }
   }
 }
